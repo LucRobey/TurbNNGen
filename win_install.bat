dd5c914f2fd20deb6854363e29d72ea38440fd65
dima1997 <franki.dimaria@gmail.com>
2023-11-16 19:23:24 +0100

    add: win_install.bat script

--- win_install.bat
+++ win_install.bat
@@ -1,5 +1,16 @@
-conda create --name turb python=3.11 --yes
-conda activate turb
-conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia --yes
-conda install conda_requirements --yes
-pip install pip_requirements.txt --yes
+@echo off
+set env_name=%1
+if "%1" == "" (
+    set env_name="turb"
+)
+echo [TURBNNGEN] Creating conda environment %env_name% ...
+CALL conda create --name turb3 python=3.11 --yes
+echo [TURBNNGEN] Activating conda environnement %env_name% ...
+CALL conda activate turb3
+echo [TURBNNGEN] Installing pytorch ...
+CALL conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia --yes
+echo [TURBNNGEN] Installing conda requirements ...
+CALL conda install conda_requirements --yes
+echo [TURBNNGEN] Installing pip requirements ...
+CALL pip install pip_requirements.txt --yes
+:: Install src folder locally
