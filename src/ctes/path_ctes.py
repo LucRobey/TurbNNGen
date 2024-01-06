d9847b01d719e688a73a9a947d29d130a0428614
FionaMrt <fionamartin.mail@gmail.com>
2024-01-06 10:08:12 +0100

    add : personal paths

--- src/ctes/path_ctes.py
+++ src/ctes/path_ctes.py
@@ -1,2 +1,4 @@
 DATAPATH = "/users/local/f22dimar/TurbNNGen/data/MRW.npz"
 SCALERPATH = "/users/local/f22dimar/TurbNNGen/data/scaler.joblib"
+DATAPATH_fm_desktop = "C:/Users/fiona/anaconda3/envs/TurbNNGen/TurbNNGen_git/data/MRW.npz"
+SCALERPATH_fm_desktop = "C:/Users/fiona/anaconda3/envs/TurbNNGen/TurbNNGen_git/data/scaler.joblib"
