ddaa301e04fe3c0783b6310fc42e2c92bcb491dc
dima1997 <franki.dimaria@gmail.com>
2024-01-12 19:20:42 +0100

    add: labels param to plot_predictions

--- src/nn/plot_predictions.py
+++ src/nn/plot_predictions.py
@@ -1,16 +1,18 @@
 import matplotlib.pyplot as plt
 
-def plot_predictions(targets, preds):
+def plot_predictions(targets, preds, labels):
     plt.figure(figsize=(10, 7))  # Adjusted figure size for a 2x2 layout
-    outputs_nb = targets.shape[1]
-    output_labels = {0: 'c1', 1: 'c2', 2: 'L', 3: 'epsilon'}
+    # outputs_nb = targets.shape[1]
+    # output_labels = {0: 'c1', 1: 'c2', 2: 'L', 3: 'epsilon'}
+    # output_labels = {i: label for i, label in enumerate(labels)}
 
-    for i in range(outputs_nb):
+    # for i in range(outputs_nb):
+    for i, label in enumerate(labels):
         plt.subplot(2, 2, i+1) 
         plt.scatter(targets[:, i], preds[:, i])
-        plt.title(f"Actual vs Predicted values of {output_labels[i]}")
-        plt.xlabel(f"Actual {output_labels[i]}")
-        plt.ylabel(f"Predicted {output_labels[i]}")
+        plt.title(f"Actual vs Predicted values of {label}")
+        plt.xlabel(f"Actual {label}")
+        plt.ylabel(f"Predicted {label}")
 
     plt.tight_layout()  
     plt.show()
