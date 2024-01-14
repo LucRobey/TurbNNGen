f03d9eac014eb5a042a453e54d0547faa4348dd8
dima1997 <franki.dimaria@gmail.com>
2024-01-14 16:53:23 +0100

    refactor: utils in results and training

---
+++ src/nn/results/utils/__init__.py
@@ -0,0 +1,4 @@
+from .analyse_predictions  import analyse_predictions
+from .get_all_predictions  import get_all_predictions
+from .plot_losses          import plot_losses
+from .plot_predictions     import plot_predictions
