45b7aaaaa25bb35b9ca4b268c7fed1481fe39bc3
dima1997 <franki.dimaria@gmail.com>
2024-03-04 14:50:31 +0100

    wip: wav2vec2

--- src/nn/archs/wav2vec2.py
+++ src/nn/archs/wav2vec2.py
@@ -7,15 +7,18 @@ import numpy as np
 import src.ctes.str_ctes as sctes
 
 class Wav2Vec2Head(nn.Module):
-    def __init__(self, in_channels, out_channels, mid_channels):
+    IN_CHANNELS = 12
+    def __init__(self, n_labels):
         super().__init__()
         self.model = self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, 8, kernel_size=3, padding=1, bias=False),
+            nn.Conv2d(self.IN_CHANNELS, 8, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(8),
             nn.ReLU(inplace=True),
-            nn.Conv2d(8, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
+            nn.Conv2d(8, n_labels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(n_labels),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten()
         )
 
     def forward(self, z):
@@ -27,19 +30,18 @@ class Wav2Vec2(nn.Module):
         https://ai.meta.com/blog/wav2vec-20-learning-the-structure-of-speech-from-raw-audio/
     for regression task
     """
-    INPUT_CHANNELS  = 1
-    IN_CHANNELS_HEAD = 12
-    def __init__(self, output_size, bundle=WAV2VEC2_ASR_BASE_960H):
+    def __init__(self, n_labels, bundle=WAV2VEC2_ASR_BASE_960H):
         super().__init__()
 
         self.wav2vec2 = bundle.get_model()
         self.wav2vec2.feature_extractor.requires_grad_(requires_grad=False)
 
-        self.wave2vec2_head = Wav2Vec2Head(self.IN_CHANNELS_HEAD, output_size)
+        self.wav2vec2_head = Wav2Vec2Head(n_labels)
             
     
     def forward(self, z):
+        z = z[:,0,:]
         features, _ = self.wav2vec2.extract_features(z)
         out = torch.stack(features, dim=1)
-        out = self.wave2vec_head(out)
+        out = self.wav2vec2_head(out)
         return out
--- src/nn/training/cnn_all_training.py
+++ src/nn/training/cnn_all_training.py
@@ -1,18 +1,18 @@
 from src.nn.training.utils      import Trainer
-from src.nn.archs               import CNN_ALL, CNN_ALL_VDCNNFRW_M18
+from src.nn.archs               import CNN_ALL, CNN_ALL_VDCNNFRW_M18, Wav2Vec2
 
 import torch
 import torch.nn                 as nn
 
 
 if __name__ == "__main__":
-    Trainer.new_training(builder=CNN_ALL_VDCNNFRW_M18, 
+    Trainer.new_training(builder=Wav2Vec2, 
                         criterion=nn.MSELoss(), 
                         optimizer_builder=torch.optim.Adam, 
                         optimizer_params={"lr":1e-4, "weight_decay": 0.0}, 
                         data_path="./data",
-                        model_name="Top-Down v2", 
-                        n_epochs=250,
+                        model_name="wav2vec2", 
+                        n_epochs=200,
                         batch_size=6, 
                         valid_size=0.2, 
                         test_size=0.2, 
